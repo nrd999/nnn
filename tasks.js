8615e2e2166d81038af3775c71d7b587c89dde54
nrd999 <104188780+nrd999@users.noreply.github.com>
2022-05-08 12:42:50 +0400

    Add files via upload

--- tasks.js
+++ tasks.js
@@ -1,40 +1,10 @@
-// 1) WHAT WILL BE THE OUTPUT AND WHY
-// let str = 'my name is ana';
-// let length = str.length - 1;
-// for (let i = 0; i < str.length; i++) {
-//    if(i === 2 || i === 7 || i === 10) {
-//        console.log(str[i] + str[length]);
-//        length--
-//    }
-// }
+// 1) Greq function vor@ vorpes argument kstana array ev ktpi tvyal arrayum exac bolor tver@;(arrayi mej karan tveric baci urish elementner el linen)
 
+// 2) Greq function vor@ kstana 2 array argument ev ktpi aveli erkar arrayi meji bolor string elementnery;
 
-// 2) WHAT WILL BE THE OUTPUT AND WHY
-// let x = 10 > 5;
-// while(x) {
-//     console.log('dont test it in console');
-//     x = !!x
-// }
+// 3) Greq function vor@ kstana erku array voric mek@ datark e, ev myus arrayi meji elementery klcni datark arrayi mej u kveradarcni ayd array@
 
-// 3) Greq nuyn code@ while-i case-i mijocov
-// for(let i = 0; i < 10; i++) {
-//     console.log(i)
-// }
+// 4) Greq function vor@ stanum a 2 argument mek@ array myus@ number type-i; ev stugum a ete arrayum ka 2rd argumentov stacvac tiv@
+// apa veradarcnum a ayd tiv@  hakarak depqum veradarcnum a false
 
-// 4) Greq nuyn code@ for-i mijocov mijocov
-// let x = 5;
-// while (x !== 10) {
-//     console.log(x);
-//     x++
-// }
-
-// 5) while cikli mijocov tpeq 10-100 mijakayqum bolor tver@
-
-// 6) for loop-i mijocov reversedName popoxakanin veragreq name popoxakan@ aynpes vor text@ lini ajic dzax grvac ev tpeq:
-// orinak ete name = 'JOHN' petq e stanaq 'NHOJ'
-
-// 7) for loop-i mijocov tpeq 1-1000 mijakayqum bolor minaish ev erknish tvery
-
-// 8) for loop-i mijocov tpeq 1-1000 mijakayqum(1-@ ev 1000-@ neraryal) bolor kent tveri gumar@ ---->>  patasxan@ petq e stanaq 250000
-
-// 9) while cikli mijocov tpeq 1-ic 10 mijakayqum gtnvox 3rd handipac zuyg tiv@;
+// 5) Greq function vor kstana 4 argument bolor@ number ev kveradarcni array vori mej klinen ayd 4 numberneri krknapatiknern
